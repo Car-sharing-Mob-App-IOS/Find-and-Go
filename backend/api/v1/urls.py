339feeb7a7387b5b061881b820733b66a15a6416
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-12-30 01:22:33 +0300

    Removed unnecessary paths added router_v1

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -2,7 +2,7 @@ from cars.views import CarViewSet
 from django.urls import include, path
 from drf_spectacular.views import SpectacularSwaggerView
 from rest_framework import routers
-from reviews.views import ReviewDetailView, ReviewListCreateView
+from reviews.views import ReviewView
 from users.views import PublicUserViewSet
 
 from .router_settings import CustomDjoserUserRouter
@@ -16,6 +16,7 @@ user_router_v1 = CustomDjoserUserRouter()
 # Register
 user_router_v1.register("users", PublicUserViewSet, "users")
 router_v1.register("cars", CarViewSet, "cars")
+router_v1.register("reviews", ReviewView, "reviews")
 
 
 # URL
@@ -23,16 +24,6 @@ urlpatterns = [
     path("", include(router_v1.urls)),
     path("", include(user_router_v1.urls)),
     path("auth/", include("djoser.urls.authtoken")),
+    path("auth/", include("djoser.social.urls")),
     path("swagger/", SpectacularSwaggerView.as_view(), name="swagger"),
-    path('auth/', include('djoser.social.urls')),
-    path("reviews/", ReviewListCreateView.as_view(
-        {
-            'get': 'list', 'post': 'create'
-            }
-            ), name="review-list-create"),
-    path("reviews/<int:pk>/", ReviewDetailView.as_view(
-        {
-            'get': 'retrieve'
-            }
-            ), name="review-detail")
 ]
