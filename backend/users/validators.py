506261eeb91763046043b8fccfd7b04022b682d7
Ольга Шполянская <olgab497@gmail.com>
2024-01-14 16:29:23 +0300

    MaxLengthLastNameValidator added

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -1,10 +1,25 @@
+from core.texts import MAX_LENGTH_LAST_NAME
 from django.core.exceptions import ValidationError
 from django.core.validators import MinLengthValidator, RegexValidator
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
+
+
+class MaxLengthLastNameValidator:
+    """Класс для валидации длины фамилии."""
+
+    def __init__(self, limit_value, message=None):
+        self.limit_value = limit_value
+        self.message = message
+
+    def __call__(self, value):
+        """Валидирует длину полученного значения."""
+        if value and len(value) > self.limit_value:
+            raise ValidationError(self.message, code='max_length', params={
+                                  'limit_value': self.limit_value})
 
 
 def name_surname_validator(value, min_length=3):
-    """Validate name and surname."""
+    """Валидирует имя и фамилию."""
     validator = RegexValidator(
         regex=r"^[a-zA-Zа-яА-Я\-\–]+$",  # Добавлены тире и дефис
         message=("Неверное значение, допускаются только буквы без пробелов."),
@@ -15,26 +30,37 @@ def name_surname_validator(value, min_length=3):
         f"длиной не менее {min_length} символов.",
     )
 
+    max_length_validator = MaxLengthLastNameValidator(
+        limit_value=MAX_LENGTH_LAST_NAME,
+        message=f"Значение поля должно быть "
+        f"длиной не более {MAX_LENGTH_LAST_NAME} символов.",
+    )
+
     validator(value)
     min_length_validator(value)
+    max_length_validator(value)
 
 
 class MaximumLengthValidator(MinLengthValidator):
+    """Класс для валидации длины пароля."""
+
     def __init__(self, max_length=18):
         self.max_length = max_length
 
     def validate(self, password, user=None):
+        """Валидирует длину пароля."""
         if len(password) > self.max_length:
             raise ValidationError(
                 _(
                     "Максимальная длина пароля - "
                     f"{self.max_length} символов."
-                    ),
+                ),
                 code="password_too_long",
                 params={"max_length": self.max_length},
             )
 
     def get_help_text(self):
+        """Возвращает текст справки о максимальной длине пароля."""
         return _(
             "Максимальная длина пароля - "
             f"{self.max_length} символов."
